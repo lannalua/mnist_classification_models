f7367b29f25fb76fc80089ca760bec1c3c074db2
Lanna Luara <lannanovaes010@gmail.com>
2025-10-27 15:19:26 -0300

    CNN - Random Search

--- cnn_rs.py
+++ cnn_rs.py
@@ -1,44 +1,46 @@
-import seaborn as sns
-from sklearn.metrics import classification_report, confusion_matrix
 from main import *
-import numpy as np
+
+import seaborn as sns
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from keras.layers import Conv2D, Input, MaxPooling2D, BatchNormalization, Flatten, Dense, Dropout
 from keras.models import Sequential
 from scikeras.wrappers import KerasClassifier
-from sklearn.model_selection import GridSearchCV
-from keras.callbacks import EarlyStopping  # Import EarlyStopping
+from sklearn.model_selection import RandomizedSearchCV
+from keras.callbacks import EarlyStopping
+import numpy as np
+from scipy.stats import uniform
+
 
 es = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
 
-
 def create_model_cnn(conv_filters=(32, 64),
-                     kernel_size=(3, 3),
+                     kernel_size=(3,3),
                      dense_units=(128,),
                      dropout_rate=0.3,
                      optimizer="adam",
                      activation="relu"
                      ):
   model = Sequential()
-  model.add(Input(shape=(28, 28, 1)))
+  model.add(Input(shape=(28,28,1)))
+
 
   for filters in conv_filters:
       model.add(Conv2D(filters, kernel_size, activation=activation))
       model.add(BatchNormalization())
-      model.add(MaxPooling2D(pool_size=(2, 2)))
+      model.add(MaxPooling2D(pool_size=(2,2)))
 
   model.add(Flatten())
   model.add(Dropout(dropout_rate))
 
   for units in dense_units:
-    model.add(Dense(units, activation=activation))
+    model.add(Dense(units,activation=activation))
 
-  model.add(Dense(10, activation="softmax"))
+  model.add(Dense(10,activation="softmax"))
 
   model.compile(optimizer=optimizer,
                 loss='categorical_crossentropy',
                 metrics=['accuracy'])
   return model
-
 
 cnn_model = KerasClassifier(
     model=create_model_cnn,
@@ -51,54 +53,49 @@ cnn_model = KerasClassifier(
     validation_split=0.1
 )
 
-param_grid = {
+param_distributions = {
     "model__conv_filters": [(32,), (32, 64)],
     "model__dense_units": [(128,), (256,)],
 
     "model__kernel_size": [(3, 3)],
-    "model__dropout_rate": [0.3],
-    "model__optimizer": ["adam"],
-    "batch_size": [128],
-    "epochs": [5],
+    "model__dropout_rate": uniform(0.2, 0.4),
+    'batch_size': [32, 64, 128],
+    'model__optimizer': ['adam', 'rmsprop','sgd'],
+    "epochs": [10],
     "callbacks": [[es]]
 }
 
-grid = GridSearchCV(estimator=cnn_model, param_grid=param_grid, cv=3)
-grid_result = grid.fit(x_train_cnn, y_train_cat)
+random_search = RandomizedSearchCV(estimator=cnn_model, param_distributions=param_distributions, cv=3)
+random_search_result = random_search.fit(x_train_cnn, y_train_cat)
 
-## Histórico de Métricas
+print(f"Melhor score (acurácia média na validação cruzada): {random_search_result.best_score_:.4f}")
+print("Melhores parâmetros encontrados:")
+print(random_search_result.best_params_)
 
-print("Melhor score:", grid_result.best_score_)
-print("Melhores parâmetros:", grid_result.best_params_)
-
-# Evaluate the best model on the test set
-best_cnn_model = grid_result.best_estimator_.model_
-test_loss, test_acc = best_cnn_model.evaluate(
-    x_test_cnn, y_test_cat, verbose=0) # type:ignore
+best_keras_model = random_search_result.best_estimator_.model_ #type: ignore
+test_loss, test_acc = best_keras_model.evaluate(x_test_cnn, y_test_cat, verbose=0)
 
 print(f"\nAcurácia do melhor modelo no conjunto de teste: {test_acc:.4f}")
-print(f"Perda do melhor modelo do conjunto de teste: {test_loss:.4f}")
+print(f"Perda do melhor modelo no conjunto de teste: {test_loss:.4f}")
 
 # Refit the best model to get the history object
-history_best_cnn = best_cnn_model.fit(x_train_cnn, y_train_cat, validation_split=0.1, epochs=5,
-                                      batch_size=grid_result.best_params_[
-                                          'batch_size'],
-                                      callbacks=[es])
+best_mlp_model = random_search_result.best_estimator_.model_  # type: ignore
+history_best_mlp = best_mlp_model.fit(x_train_cnn, y_train_cat, validation_split=0.1,epochs=12,
+                                     batch_size=random_search_result.best_params_['batch_size'],
+                                     callbacks=[es])
 
-train_loss_best = history_best_cnn.history['loss']
-val_loss_best = history_best_cnn.history['val_loss']
-train_acc_best = history_best_cnn.history['accuracy']
-val_acc_best = history_best_cnn.history['val_accuracy']
+train_loss_best = history_best_mlp.history['loss']
+val_loss_best = history_best_mlp.history['val_loss']
+train_acc_best = history_best_mlp.history['accuracy']
+val_acc_best = history_best_mlp.history['val_accuracy']
 
+print("\n--- Análise Detalhada do Melhor Modelo MLP no Conjunto de Teste ---")
 
-# 1. Fazer previsões para obter as classes
-y_pred_proba = best_cnn_model.predict(x_test_cnn)
+y_pred_proba = best_keras_model.predict(x_test)
 y_pred = np.argmax(y_pred_proba, axis=1)
 
-# 2. Converter os rótulos de teste (que estão em one-hot encoding) de volta para 1D
 y_test_labels = np.argmax(y_test_cat, axis=1)
 
-# Acurácia, Precisão, Sensibilidade (Recall) e F1-Score
 print("\n--- Relatório de Classificação ---")
 print(classification_report(y_test_labels, y_pred))
 
@@ -124,6 +121,7 @@ for i in range(num_classes):
     specificity = tn / (tn + fp)
 
     print(f"Especificidade para a Classe {i}: {specificity:.4f}")
+    print("-"*15)
 
 epochs_best = range(len(train_loss_best))
 
@@ -132,7 +130,7 @@ fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(18, 5))
 # Gráfico (a) Loss
 ax1.plot(epochs_best, train_loss_best, label='Train')
 ax1.plot(epochs_best, val_loss_best, label='Validation')
-ax1.set_title('Loss do Melhor Modelo MLP')
+ax1.set_title('Loss do Melhor Modelo CNN')
 ax1.set_xlabel('epoch\n\n(a) Loss')
 ax1.legend()
 
